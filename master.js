22f153ed73eb461d8adfb54a6b9037c087644c59
Ben Stav <benstav@gmail.com>
2012-03-21 23:16:01 +0200

    Changed /cowsay to listener, made it much better

--- master.js
+++ master.js
@@ -2146,94 +2146,127 @@ return function () {
 
 ;
 (function () {
+var defaults = {
+	e : 'oo',
+	T : '  ',
+	t : false,
+	W : 40
+};
 
-//in the "template", E is for eye, T for Tongue
-//it looks more like a donkey who was involved in a sledgehammer accident
-// because of escaping and newlines
-var cow = "\n\
-        \\   ^__^\n\
-         \\  (E)\\_______\n\
-            (__)\\       )\\/\\\n\
-             T ||----w |\n\
-                ||     ||\
-";
+var cowsay = {
 
+	//in the "template", e is for eye, T for Tongue, L for bubble-Line
+	//it looks more like a donkey who was involved in a sledgehammer accident
+	// because of escaping and newlines
+	//the cow business is a dangerous one
+	cow : [
+		'',
+		'        L   ^__^',
+		'         L  (e)\\_______',
+		'            (__)\\       )\\/\\',
+		'             T ||----w |',
+		'                ||     ||'
+	].join( '\n' ),
 
-function prettyBalloon ( message ) {
-	message = message.trim().split( '\n' );
-	var longest = Math.max.apply(
-		Math,
-		message.map(function ( line ) {
-			return line.length;
-		})
-	),
-	//for the top and bottom lines of the thought bubble
-		boundaryOccurences = new Array( longest + 2 ),
-		topLine = boundaryOccurences.join( '_' ),
-		btmLine = boundaryOccurences.join( '-' ),
+	moo : function ( message, opts ) {
+		this.eyes     = opts.e.slice( 0, 2 );
+		this.tongue   = opts.T.slice( 0, 2 );
+		this.line     = opts.t ? 'O' : '\\';
+		this.thinking = opts.t;
 
-	//the border of the speech bubble
-		border,
-		walls = '',
-		lineCount = message.length;
+		this.message  = wordWrap( message, opts.W );
 
-	switch ( lineCount ) {
-	case 1:
-		border = [ '<', '>' ];
-		break;
-	//intentional weirdness
-	default:
-		walls = '|';
-	//intentional convuluted fallthrough
-	case 2:
-		border = [ '/', '\\', '\\', '/' ];
-	}
+		//cowsay is actually the result of breeding a balloon and a cow
+		return this.makeBalloon() + this.makeCow();
+	},
 
-	var balloon = message.map(function ( line, idx ) {
-		var padders;
-		//top left and top right
-		if ( idx === 0 ) {
-			padders = border.slice( 0, 2 );
+	makeCow : function () {
+		return this.cow
+			.replace( 'e', this.eyes )
+			.replace( 'T', this.tongue )
+			.replace( new RegExp('L', 'g'), this.line );
+	},
+
+	makeBalloon : function () {
+		var message = this.message.trim().split( '\n' );
+
+		var longest = Math.max.apply(
+			Math,
+			message.map(function ( line ) {
+				return line.length;
+			})
+		),
+
+		//for the top and bottom lines of the thought bubble
+			boundaryOccurences = new Array( longest + 2 ),
+			topLine = boundaryOccurences.join( '_' ),
+			btmLine = boundaryOccurences.join( '-' ),
+
+			lineCount = message.length,
+		//the border of the speech bubble
+			border = this.chooseBorders( lineCount );
+
+		var balloon = message.map(function ( line, idx ) {
+			var padders;
+			//top left and top right
+			if ( idx === 0 ) {
+				padders = border.slice( 0, 2 );
+			}
+			//bottom left and bottom right
+			else if ( idx === lineCount-1 ) {
+				padders = border.slice( 2, 4 );
+			}
+			//the wall
+			else {
+				padders = border.slice( -2 );
+			}
+
+			return padders[ 0 ] + ' ' +
+				rightPad( line, longest ) + ' ' +
+				padders[ 1 ];
+		});
+
+		balloon.unshift( ' ' + topLine );
+		balloon.push( ' ' + btmLine );
+
+		return balloon.join( '\n' );
+	},
+
+	chooseBorders : function ( lineCount ) {
+		if ( this.thinking ) {
+			return [ '(', ')', '(', ')', '(', ')' ];
 		}
-		//bottom left and bottom right
-		else if ( idx === lineCount-1 ) {
-			padders = border.slice( -2 );
+
+		var border;
+		if ( lineCount === 1 ) {
+			border = [ '<', '>' ];
 		}
-		//the wall
+		//good enough
 		else {
-			padders = [ walls, walls ];
+			border = [ '/', '\\', '\\', '/', '|', '|' ];
 		}
 
-		return padders[ 0 ] + ' ' +
-			rightPad( line, longest ) + ' ' +
-			padders[ 1 ];
-	});
+		return border;
+	}
+};
 
-	balloon.unshift( ' ' + topLine );
-	balloon.push( ' ' + btmLine );
-
-	return balloon.join( '\n' );
-}
 
 function wordWrap ( str, len ) {
-	var line = '',
+	var lineLen = 0,
 		ret = [];
 
-	str.split( ' ' ).forEach(function ( word ) {
+	return str.split( ' ' ).reduce(function ( ret, word ) {
 		var wordLen = word.length;
 
 		//let the wrapping...commence!
-		if ( wordLen + line.length > len ) {
-			ret.push( line );
-			line = '';
+		if ( lineLen + wordLen > len ) {
+			ret += '\n';
+			lineLen = 0;
 		}
+		lineLen += wordLen + 1; //+1 for the space we now add
 
-		line += word + ' ';
-	});
-
-	ret.push( line );
-
-	return ret.join( '\n' );
+		return ret + word + ' ';
+	}, '' );
 }
 
 function rightPad ( str, len, padder ) {
@@ -2244,31 +2277,32 @@ function rightPad ( str, len, padder ) {
 	return str;
 }
 
-function cowsay ( message, len, eyes, tongue ) {
-	len = len || 40;
-	eyes = ( eyes || 'oo' ).slice( 0, 2 );
-	tongue = ( tongue || '  ' ).slice( 0, 2 );
+bot.listen(
+	/cow(think|say)\s(?:([eT])=(.{0,2})\s)?(?:([eT])=(.{0,2})\s)?(.+)/,
 
-	return prettyBalloon( wordWrap(message, len) ) +
-		cow.replace( 'E', eyes ).replace( 'T', tongue );
-}
+	function ( msg ) {
+		//the first item is the whole match, second item is the "think" or
+		// "say", last item is the message, we only want the "parameters"
+		var args = msg.matches.slice( 2, -1 ),
+			opts = {};
 
-bot.addCommand({
-	name : 'cowsay',
-	fun : function ( args ) {
-		if ( !args.content ) {
-			return 'Telepathic module not loaded, please specify your demands' +
-				' in textual format.';
+		for ( var i = 0, len = args.length; i < len; i += 2 ) {
+			//if that capturing group got something,
+			if ( args[i] && args[i+1] ) {
+				//set that parameter
+				opts[ args[i] ] = args[ i + 1 ];
+			}
 		}
-		args.respond(
-			args.codify( cowsay(args.content) )
-		);
-	},
-	permissions : {
-		del : 'NONE'
-	},
-	description : 'Make moo'
-});
+		console.log( opts );
+		//cowsay or cowthink?
+		opts.t = msg.matches[ 1 ] === 'think';
+		opts = Object.merge( defaults, opts );
+		console.log( opts );
+
+		var cowreact = cowsay.moo( msg.matches.slice(-1)[0], opts );
+		msg.respond( msg.codify(cowreact) );
+	}
+);
 
 }());
 
