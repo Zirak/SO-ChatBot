658c8af34e081cafd93dbec408bcf48249542690
Ben Stav <benstav@gmail.com>
2012-05-18 19:01:45 +0300

    And of course, forgot to build

--- master.js
+++ master.js
@@ -2222,6 +2222,53 @@ what              --simply the word what
 }());
 
 ;
+(function () {
+
+function makeWorkerExecuteSomeCode( code, callback ) {
+    var timeout;
+
+    code = code + "";
+    var worker = new Worker( 'codeWorker.js' );
+
+    worker.addEventListener( "message", function(event) {
+        clearTimeout(timeout);
+        callback( event.data );
+    });
+
+    worker.postMessage({
+        code: code
+    });
+
+    timeout = window.setTimeout( function() {
+        callback( "Maximum execution time exceeded" );
+        worker.terminate();
+    }, 1000 );
+}
+
+bot.listener(
+	/^\>(.+)$/,
+	function ( msg ) {
+		var maxLen = 1024;
+
+		makeWorkerExecuteSomeCode( msg.matches[1], finish );
+
+		function finish ( answer ) {
+			if ( answer.length > maxLen ) {
+				answer = '(snipped)' + answer.slice( 0, maxLen );
+			}
+
+			msg.directreply( answer );
+		}
+	}
+);
+
+}());
+
+;
+
+;
+
+;
 var moo = (function () {
 
 var cowsay = {
