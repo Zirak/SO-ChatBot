f311601e780568e7455eaef8d50ac272c355cd56
Ben Stav <benstav@gmail.com>
2012-06-28 15:49:03 +0300

    ...I don't wanna talk about it

--- master.js
+++ master.js
@@ -319,12 +319,12 @@ var bot = window.bot = {
 			console.error( e, err );
 		}
 	},
-	
+
 	prepareMessage : function ( msgObj ) {
 		msgObj = this.adapter.transform( msgObj );
 
 		var msg = IO.decodehtmlEntities( msgObj.content );
-		
+
 		return this.Message(
 			msg.slice( this.invocationPattern.length ).trim(),
 			msgObj
@@ -439,7 +439,8 @@ var bot = window.bot = {
 				if ( resp ) {
 					msg.reply( resp );
 				}
-				else if ( resp !== false ) {
+
+				if ( resp !== false ) {
 					fired = true;
 				}
 			}
@@ -2217,7 +2218,7 @@ bot.listen( dictionaries, function ( msg ) {
 });
 /*
 what              --simply the word what
-(?:\'s)?          --optional 's suffix (what's)
+(?:'s|'re)?       --optional suffix (what's, what're)
 \s
 (?:
     (?:is|are)    --is|are
