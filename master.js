7a857ca3041394d4abe66e1fa880ff179a7ffb5b
Zirak <benstav@gmail.com>
2012-08-17 02:18:07 +0300

    ...and it brought amnesia!

--- master.js
+++ master.js
@@ -826,7 +826,7 @@ Math.ratio = function ( a, b ) {
     b = Number( b );
 
     var gcd = this.gcd( a, b );
-    return ( a / gcd ) + ':' + ( a / gcd );
+    return ( a / gcd ) + ':' + ( b / gcd );
 };
 
 //Euclidean gcd
@@ -842,9 +842,9 @@ String.prototype.supplant = function ( obj ) {
 	return this.replace( /\{([^\}]+)\}/g, replace );
 
 	function replace ( $0, $1 ) {
-		return obj[ $1 ] ?
+		return obj.hasOwnProperty( $1 ) ?
 			obj[ $1 ] :
-			$1;
+			$0;
 	}
 };
 
@@ -5756,10 +5756,11 @@ bot.addCommand({
 ;
 (function () {
 
-var template = '[{display_name}]({link})'   +
-		'has {reputation} reputation, '     +
-		'asked {question_count} questions,' +
-		'gave {answer_count} answers, '     +
+var template = '[{display_name}]({link}) '          +
+		'has {reputation} reputation, '             +
+		'earned {reputation_change_day} rep today, '+
+		'asked {question_count} questions, '        +
+		'gave {answer_count} answers, '             +
 		'for a q:a ratio of {ratio}';
 
 function stat ( msg, cb ) {
@@ -5770,9 +5771,15 @@ function stat ( msg, cb ) {
 		id = msg.findUserid( id );
 	}
 
+	//~10% chance
+	if ( Math.random() <= 0.1 ) {
+		finish( 'That dude sucks' );
+		return;
+	}
+
 	IO.jsonp({
 		url : 'https://api.stackexchange.com/2.0/users/' + id,
-		params : {
+		data : {
 			site   : 'stackoverflow',
 			filter :  '!G*klMsSp1IcBUKxXMwhRe8TaI(' //ugh, don't ask...
 		},
@@ -5780,12 +5787,19 @@ function stat ( msg, cb ) {
 	});
 
 	function done ( resp ) {
+		if ( resp.error_message ) {
+			finish( resp.error_message );
+			return;
+		}
+
 		var user = resp.items[ 0 ], res;
 
 		if ( !user ) {
 			res = 'User ' + id + ' not found';
 		}
 		else {
+			user = normalize_stats( user );
+			console.log( user, '/stat normalized' );
 			res = template.supplant( user );
 		}
 
@@ -5802,6 +5816,29 @@ function stat ( msg, cb ) {
 	}
 }
 
+function normalize_stats ( stats ) {
+	stats = Object.merge(
+		{
+			question_count        : 0,
+			answer_count          : 0,
+			reputation_change_day : 0
+		},
+		stats );
+
+	//for teh lulz
+	if ( !stats.question_count && stats.answer_count ) {
+		stats.ratio = "H̸̡̪̯ͨ͊̽̅̾̎Ȩ̬̩̾͛ͪ̈́̀́͘ ̶̧̨̱̹̭̯ͧ̾ͬC̷̙̲̝͖ͭ̏ͥͮ͟Oͮ͏̮̪̝͍M̲̖͊̒ͪͩͬ̚̚͜Ȇ̴̟̟͙̞ͩ͌͝S̨̥̫͎̭ͯ̿̔̀ͅ";
+	}
+	else if ( !stats.answer_count && stats.question_count ) {
+		stats.ratio = "TO͇̹̺ͅƝ̴ȳ̳ TH̘Ë͖́̉ ͠P̯͍̭O̚​N̐Y̡";
+	}
+	else {
+		stats.ratio = Math.ratio( stats.question_count, stats.answer_count );
+	}
+
+	return stats;
+}
+
 bot.addCommand({
 	name : 'stat',
 	fun : stat,
@@ -5809,10 +5846,12 @@ bot.addCommand({
 		del : 'NONE'
 	},
 
-	description : 'Gives useless stats on a user. `/stat usrid|usrname`'
+	description : 'Gives useless stats on a user. `/stat usrid|usrname`',
+	async : true
 });
 
 }());
+
 
 ;
 
