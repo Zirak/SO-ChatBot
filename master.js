3acee6876a4999fc7a79e1bd85cb34ee11ab5bff
Zirak <benstav@gmail.com>
2012-11-24 16:35:27 +0200

    ~_~

--- master.js
+++ master.js
@@ -2611,6 +2611,22 @@ function nudgeListener ( args ) {
 
 ;
 (function () {
+var resps = [
+	'<3', ':D', 'yummy!', '*nom nom nom*' ],
+	re = /^botsnack/;
+var botsnack = resps.random.bind( resps );
+
+bot.listen( re, botsnack );
+IO.register( 'input', function ( msgObj ) {
+	if ( re.test(msgObj.content) ) {
+		bot.reply( botsnack(), msgObj );
+	}
+});
+
+}());
+
+;
+(function () {
 //var rings = {
 //   roomid : [ members ]
 //}
