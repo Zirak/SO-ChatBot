03824687ce987b55eaf88bfb651e117ebd48ffed
Zirak <benstav@gmail.com>
2012-12-29 01:44:46 +0200

    added timer plugin; fixes #29

--- master.js
+++ master.js
@@ -3273,6 +3273,59 @@ bot.addCommand({
 
 ;
 (function () {
+var timers = Object.create( null ),
+	id = 0;
+
+var actions = {
+	start : function ( name ) {
+		timers[ name ] = Date.now();
+		return 'Registered timer ' + name;
+	},
+
+	stop : function ( name ) {
+		var timer = timers[ name ];
+
+		if ( !timer ) {
+			return 'I have no knowledge of timer ' + name;
+		}
+
+		var delta = Date.now() - timer;
+		delete timers[ name ];
+
+		return delta + 'ms';
+	}
+};
+
+function timer ( msg ) {
+	var args = msg.parse(),
+		act = args.shift(),
+		//if Crockford ever reads this, I want to reassure him: I did mean
+		// postfix increment. I want to grab the original value of id while
+		// increasing its value
+		//now you may continue reading the code at ease
+		name = args.shift() || (id++);
+
+	if ( !actions[act] ) {
+		return 'Action {0} not recognized, see `/help timer`'.supplant( act );
+	}
+	return actions[ act ]( name );
+};
+
+bot.addCommand({
+	name : 'timer',
+	fun  : timer,
+	permissions : {
+		del : 'NONE'
+	},
+	description : 'Starts/stops a timer. ' +
+		'`/timer start [name]` starts a timer, ' +
+		'`/timer stop [name]` stops a timer.'
+});
+
+})();
+
+;
+(function () {
 
 var template = '[{display_name}]({link}) '           +
 		'has {reputation} reputation, '              +
