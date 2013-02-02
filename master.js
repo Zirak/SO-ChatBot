e14066844748265c4dd373595f3d72c1d43794f3
Zirak <benstav@gmail.com>
2013-02-03 01:28:45 +0200

    HALT, HAMMERZEIT!, as proposed by @GNi33

--- master.js
+++ master.js
@@ -2690,10 +2690,16 @@ bot.adapter.init();
 
 ;
 IO.register( 'input', function ( msgObj ) {
-	var words = msgObj.content.match( /\w+/g ) || [];
+	var sentence = msgObj.content.toUpperCase();
 
-	if ( words.length === 1 && words[0].toUpperCase() === 'STOP' ) {
+	//for probably good reason, it didn't allow me to apply the optional
+	// operator on beginnin-of-input, i.e. ^?
+	//so we have to wrap the ^ in parens
+	if ( /(^)?STOP[\.!\?]?$/.test(sentence) ) {
 		bot.adapter.out.add( 'HAMMERTIME!', msgObj.room_id );
+	}
+	else if ( /(^)?HALT[\.!\?]?$/.test(sentence) ) {
+		bot.adapter.out.add( 'HAMMERZEIT!', msgObj.room_id );
 	}
 });
 
