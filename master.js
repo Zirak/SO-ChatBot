f263098aa9991dbdfa8ef7d9b89b9f69b39b026e
Zirak <benstav@gmail.com>
2013-02-05 01:49:47 +0200

    fuck, forgot to build...again

--- master.js
+++ master.js
@@ -3522,6 +3522,14 @@ bot.addCommand({
 }());
 
 ;
+IO.register( 'input', function ( msgObj ) {
+	if ( msgObj.user_id === 1348195 && Math.random() < 0.01 ) {
+		bot.adapter.out.add(
+			bot.adapter.reply(msgObj.user_name) + ' The Game' );
+	}
+});
+
+;
 (function () {
 
 var template = '[{display_name}]({link}) '           +
