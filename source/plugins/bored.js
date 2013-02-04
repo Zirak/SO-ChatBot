df3977ac796249c16b3685afcae39c623668ec4d
Zirak <benstav@gmail.com>
2013-02-05 01:46:30 +0200

    fixed boredom. I shall remember remember...

--- source/plugins/bored.js
+++ source/plugins/bored.js
@@ -1,17 +1,11 @@
 //when nothing happens after a while, I get bored.
 (function () {
-var run = false,
-	lastIdx = null,
-	delay = 300000; //1000(ms) * 60 (sec) * 5 = 5min
+var delay = 300000; //1000(ms) * 60 (sec) * 5 = 5min
 
 function zzz () {
-	if ( !run ) {
-		return;
-	}
-	run = false;
 	var now = Date.now(),
-		obj = bot.adapter.in.lastTimes;
-	Object.keys( obj ).filter( timeCheck ).forEach( stuff );
+		times = bot.adapter.in.lastTimes;
+	Object.keys( times ).filter( timeCheck ).forEach( stuff );
 
 	//let my naming expertise astound you once more
 	function stuff ( roomid ) {
@@ -20,7 +14,7 @@ function zzz () {
 	}
 
 	function timeCheck ( roomid ) {
-		return obj[ roomid ] + delay <= now;
+		return times[ roomid ] + delay <= now;
 	}
 }
 IO.register( 'heartbeat', zzz );
