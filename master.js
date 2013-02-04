d52f199b5877718fa6d2cf5900a7c842cc8c3a4e
Zirak <benstav@gmail.com>
2013-02-05 01:53:59 +0200

    whoops, wrong target

--- master.js
+++ master.js
@@ -3523,7 +3523,7 @@ bot.addCommand({
 
 ;
 IO.register( 'input', function ( msgObj ) {
-	if ( msgObj.user_id === 1348195 && Math.random() < 0.01 ) {
+	if ( msgObj.user_id === 1386886 && Math.random() < 0.005 ) {
 		bot.adapter.out.add(
 			bot.adapter.reply(msgObj.user_name) + ' The Game' );
 	}
--- source/plugins/vendetta.js
+++ source/plugins/vendetta.js
@@ -1,5 +1,5 @@
 IO.register( 'input', function ( msgObj ) {
-	if ( msgObj.user_id === 1348195 && Math.random() < 0.01 ) {
+	if ( msgObj.user_id === 1386886 && Math.random() < 0.005 ) {
 		bot.adapter.out.add(
 			bot.adapter.reply(msgObj.user_name) + ' The Game' );
 	}
