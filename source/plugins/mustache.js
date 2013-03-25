d6302f250816ec4e9d77288706a1e38cfe333541
Zirak <benstav@gmail.com>
2013-03-25 14:36:22 +0200

    + /mustache per request

---
+++ source/plugins/mustache.js
@@ -0,0 +1,32 @@
+(function () {
+
+function mustachify ( args ) {
+	var usrid = args.content;
+
+	if ( !usrid ) {
+		usrid = args.get( 'user_id' );
+	}
+	else if ( /\D/.test(usrid) ) {
+		usrid = args.findUserid( usrid );
+	}
+
+	bot.log( usrid, '/mustache mapped' );
+
+	if ( usrid < 0 || !bot.users.hasOwnProperty(usrid) ) {
+		return 'User {0} was not found.'.supplant( usrid );
+	}
+
+	return 'http://mustachify.me/?src=http://www.gravatar.com/avatar/{0}?s=256#.jpg'.supplant( bot.users[usrid].email_hash );
+}
+
+bot.addCommand({
+	name : 'mustache',
+	fun : mustachify,
+	privileges : {
+		del : 'NONE'
+	},
+
+	description : 'Mustachifies a user. `/mustache [usrid|user name]`'
+});
+
+}());
