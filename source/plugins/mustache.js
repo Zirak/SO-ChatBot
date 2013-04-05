cab1d58c23b2592552f4c652373027f0ad7629b5
Zirak <benstav@gmail.com>
2013-04-05 23:20:19 +0300

    added mustache url handling

--- source/plugins/mustache.js
+++ source/plugins/mustache.js
@@ -1,7 +1,14 @@
 (function () {
+"use strict";
 
 function mustachify ( args ) {
 	var usrid = args.content;
+
+	//check for url passing
+	if ( linkCheck(usrid) ) {
+		finish( encodeURIComponent(usrid) );
+		return;
+	}
 
 	if ( !usrid ) {
 		usrid = args.get( 'user_id' );
@@ -16,9 +23,28 @@ function mustachify ( args ) {
 		return 'User {0} was not found.'.supplant( usrid );
 	}
 
-	args.directreply(
-		'http://mustachify.me/?src=http://www.gravatar.com/avatar/{0}?s=256#.jpg'
-			.supplant( bot.users[usrid].email_hash ) );
+	var hash = bot.users[ usrid ].email_hash;
+	//SO now allows non-gravatar images. the email_hash will be a link to the
+	// image in that case, prepended with a ! for some reason
+	if ( hash[0] === '!' ) {
+		finish( encodeURIComponent(hash.slice(1)) );
+	}
+	else {
+		finish(
+			'http%3A%2F%2Fwww.gravatar.com%2Favatar%2F{0}%3Fs%3D256%26d%3Didenticon#.png'.supplant(hash) );
+	}
+
+	function finish ( src ) {
+		bot.log( src, '/mustache finish' );
+
+		args.directreply(
+			'http://mustachify.me/?src=' + src );
+	}
+}
+
+function linkCheck ( suspect ) {
+	return ( suspect.startsWith('http') || suspect.startsWith('www') ) &&
+		/png|gif|jpe?g$/.test( suspect );
 }
 
 bot.addCommand({
@@ -28,7 +54,7 @@ bot.addCommand({
 		del : 'NONE'
 	},
 
-	description : 'Mustachifies a user. `/mustache [usrid|user name]`'
+	description : 'Mustachifies a user. `/mustache [link|usrid|user name]`'
 });
 
 }());
