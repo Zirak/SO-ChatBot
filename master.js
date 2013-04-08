90a0edfd72eddcfc8301244ae700294420d7cd2c
Octavian Damiean <odamiean@linux.com>
2013-04-08 02:04:24 +0200

    Fixed weasel plugin. Added case-insensitive flag to new RegExp object.

--- master.js
+++ master.js
@@ -7149,7 +7149,7 @@ var chooseRe = /^\s*(choose|should)?.*\sor\s[^$]/i,
 	questionRe = new RegExp([
 		"am", "are", "can", "could", "do", "does", "is", "may", "might",
 		"shall", "should", "will", "would"
-	].map(RegExp.escape).join('|'));
+	].map(RegExp.escape).join('|'), 'i');
 
 //personal pronouns to capitalize and their mapping
 //TODO: add possessives (should my cat => your cat should)
