0180f17b19a73803f693918d473524ea88870fff
Zirak <benstav@gmail.com>
2013-05-05 13:38:51 +0300

    damn one-indexing...

--- source/plugins/firefly.js
+++ source/plugins/firefly.js
@@ -4,5 +4,5 @@ bot.listen( /(which |what |give me a )?firefly ep(isode)?/i, function () {
 	var names = ["Serenity", "The Train Job", "Bushwhacked", "Shindig", "Safe", "Our Mrs. Reynolds", "Jaynestown", "Out of Gas", "Ariel", "War Stories", "Trash", "The Message", "Heart of Gold", "Objects in Space"];
 
 	var r = Math.floor(Math.random() * 14);
-	return 'Episode {0} - {1}'.supplant(r, names[r]);
+	return 'Episode {0} - {1}'.supplant(r + 1, names[r]);
 });
