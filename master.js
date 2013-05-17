f29fa383365ec8a69aca19c700f3a532ac835c6d
Zirak <benstav@gmail.com>
2013-05-17 12:59:40 +0300

    removed and twinked some bored responses (#54, #58)

--- master.js
+++ master.js
@@ -5177,7 +5177,6 @@ responses = [
 	'http://stackoverflow.com/a/778275/617762',
 	'http://stackoverflow.com/users/22656/jon-skeet',
 
-
 	'I really hate it when people don\'t',
 	'Accordion to recent surveys, you may insert random instrument names into' +
 		' sentences without people noticing',
@@ -5189,15 +5188,10 @@ responses = [
 		'To answer it.', //what did you expect? perv.
 
 	//stolen from copy (ha!)
-	'We all know Linux is great...it does infinite loops in 5 seconds. ~ Linus Torvalds',
-	'Everything should be made as simple as possible, but not simpler. ~ Albert Einstein',
-	'If A = B and B = C, then A = C, except where void or prohibited by law. ~ Roy Santoro',
+	'> We all know Linux is great...it does infinite loops in 5 seconds. ~ Linus Torvalds',
+	'> Everything should be made as simple as possible, but not simpler. ~ Albert Einstein',
+	'> If A = B and B = C, then A = C, except where void or prohibited by law. ~ Roy Santoro',
 	'Has anyone really been far even as decided to use even go want to do look more like?',
-
-	//nice snippets
-	'    Math.sign = function (x) {\n' +
-	'        return (x > 0) - (x < 0);\n' +
-	'    }',
 
 	//anti-jokes start here
 	'Why can\'t Elvis Presley drive in reverse? Because he\'s dead',
@@ -5214,14 +5208,10 @@ responses = [
 		'they\'re getting divorced',
 	'An Irishman, a homosexual and a Jew walk into a bar. What a fine ' +
 		'example of an integrated community',
-	'What\'s worse than biting on an apple and finding a worm? The holocaust',
 	'*knock knock* Who\'s there? "Jehova\'s witnesses"',
 	'How do you make a plumber cry? Kill his family',
 	'What did the farmer say when he couldn\'t find his tractor?\n' +
 		'"Where\'s my tractor?"',
-
-	//added by rlemon
-	'@jAndy, THE GAME!',
 
 	//added by dystroy
 	"You should always put at the start of your HTML files a comment for browsers that don't handle HTML",
@@ -5242,61 +5232,52 @@ responses = [
 	"Many noobs confuse JSON and js objects. For exemple John Resig called getJSON a function returning a plain javascript object.",
 	'"use strict" is for sissies',
 	"How do you make a plumber cry ? And why ?",
-	'There are two ways of constructing a software design: One way is to make it so simple that there are obviously no deficiencies and the other way is to make it so complicated that there are no obvious deficiencies. [C.A.R. Hoare]',
-	'The cheapest, fastest, and most reliable components are those that aren’t there. [Gordon Bell]',
-	'Deleted code is debugged code. [Jeff Sickel]',
-	'Beauty is more important in computing than anywhere else in technology because software is so complicated. Beauty is the ultimate defence against complexity. [David Gelernter]',
-	'Simplicity is prerequisite for reliability. [Edsger W. Dijkstra]',
+	'> There are two ways of constructing a software design: One way is to make it so simple that there are obviously no deficiencies and the other way is to make it so complicated that there are no obvious deficiencies. [C.A.R. Hoare]',
+	'> The cheapest, fastest, and most reliable components are those that aren’t there. [Gordon Bell]',
+	'> Deleted code is debugged code. [Jeff Sickel]',
+	'> Beauty is more important in computing than anywhere else in technology because software is so complicated. Beauty is the ultimate defence against complexity. [David Gelernter]',
+	'> Simplicity is prerequisite for reliability. [Edsger W. Dijkstra]',
 	'PHP is a minor evil perpetrated and created by incompetent amateurs, whereas Perl is a great and insidious evil perpetrated by skilled but perverted professionals',
-	'The only places for icons is in a church, a burning church at that. [mhat]',
-	'Measuring programming progress by lines of code is like measuring aircraft building progress by weight. [Bill Gates]',
-	'The object-oriented model makes it easy to build up programs by accretion. What this often means, in practice, is that it provides a structured way to write spaghetti code. [Paul Graham]',
-	'Most software today is very much like an Egyptian pyramid with millions of bricks piled on top of each other, with no structural integrity, but just done by brute force and thousands of slaves. [Alan Kay]',
-	'A language that doesn’t have everything is actually easier to program in than some that do. [Dennis M. Ritchie]',
-	'Mostly, when you see programmers, they aren’t doing anything. One of the attractive things about programmers is that you cannot tell whether or not they are working simply by looking at them. Very often they’re sitting there seemingly drinking coffee and gossiping, or just staring into space. What the programmer is trying to do is get a handle on all the individual and unrelated ideas that are scampering around in his head. [Charles M. Strauss]',
-	'You can’t trust code that you did not totally create yourself. [Ken Thompson]',
-	'Object-oriented design is the roman numerals of computing. [Rob Pike]',
-	'If you want to go somewhere, goto is the best way to get there. [ken]',
-	'Simplicity is the ultimate sophistication. [Leonardo da Vinci]',
+	'> The only places for icons is in a church, a burning church at that. [mhat]',
+	'> Measuring programming progress by lines of code is like measuring aircraft building progress by weight. [Bill Gates]',
+	'> The object-oriented model makes it easy to build up programs by accretion. What this often means, in practice, is that it provides a structured way to write spaghetti code. [Paul Graham]',
+	'> Most software today is very much like an Egyptian pyramid with millions of bricks piled on top of each other, with no structural integrity, but just done by brute force and thousands of slaves. [Alan Kay]',
+	'> A language that doesn’t have everything is actually easier to program in than some that do. [Dennis M. Ritchie]',
+	'> Mostly, when you see programmers, they aren’t doing anything. One of the attractive things about programmers is that you cannot tell whether or not they are working simply by looking at them. Very often they’re sitting there seemingly drinking coffee and gossiping, or just staring into space. What the programmer is trying to do is get a handle on all the individual and unrelated ideas that are scampering around in his head. [Charles M. Strauss]',
+	'> You can’t trust code that you did not totally create yourself. [Ken Thompson]',
+	'> Object-oriented design is the roman numerals of computing. [Rob Pike]',
+	'> If you want to go somewhere, goto is the best way to get there. [ken]',
+	'> Simplicity is the ultimate sophistication. [Leonardo da Vinci]',
 	'Every language has an optimization operator. In C++ that operator is ‘//’',
-	'“Should array indices start at 0 or 1? My compromise of 0.5 was rejected without, I thought, proper consideration. ” - Stan Kelly-Bootle',
-	'“ Always code as if the guy who ends up maintaining your code will be a violent psychopath who knows where you live. ” - Rick Osborne',
-	'“ Any fool can write code that a computer can understand. Good programmers write code that humans can understand. ” - Martin Fowler',
-	'“ Beware of bugs in the above code; I have only proved it correct, not tried it. ” - Donald Knuth',
-	'If we’d asked the customers what they wanted, they would have said “faster horses” — Henry Ford',
-	'I (…) am rarely happier than when spending an entire day programming my computer to perform automatically a task that would otherwise take me a good ten seconds to do by hand.  Douglas Adams, Last Chance to See',
-	'I will, in fact, claim that the difference between a bad programmer and a good one is whether he considers his code or his data structures more important. Bad programmers worry about the code. Good programmers worry about data structures and their relationships.  — Linus Torvalds',
-	'Some problems are so complex that you have to be highly intelligent and well informed just to be undecided about them. — Laurence J. Peter',
-	'Any intelligent fool can make things bigger, more complex, and more violent. It takes a touch of genius — and a lot of courage — to move in the opposite direction. — Albert Einstein',
-	'Software obeys the law of gaseous expansion – it continues to grow until memory is completely filled. — Larry Gleason',
-	'An organisation that treats its programmers as morons will soon have programmers that are willing and able to act like morons only. — Bjarne Stroustrup',
-	'I have always wished that my computer would be as easy to use as my telephone. My wish has come true. I no longer know how to use my telephone. — Bjarne Stroustrup',
-	'As soon as we started programming, we found to our surprise that it wasn’t as easy to get programs right as we had thought. Debugging had to be discovered. I can remember the exact instant when I realized that a large part of my life from then on was going to be spent in finding mistakes in my own programs.  — Maurice Wilkes discovers debugging, 1949',
-	'Software gets slower faster than hardware gets faster. — Wirth’s law',
-	'OOP is to writing a program, what going through airport security is to flying. — Richard Mansfield',
-	'The problem with object-oriented languages is they’ve got all this implicit environment that they carry around with them. You wanted a banana but what you got was a gorilla holding the banana and the entire jungle. — Joe Armstrong',
-	'IDE features are language smells. — Reg Braithwaite',
-	'When there is no type hierarchy you don’t have to manage the type hierarchy. — Rob Pike',
-	'Languages that try to disallow idiocy become themselves idiotic. — Rob Pike',
-	'Premature optimization, that’s like a fart. Premature abstraction is like taking a dump on another developer’s desk.  Chris Eric',
+	'> Should array indices start at 0 or 1? My compromise of 0.5 was rejected without, I thought, proper consideration. - Stan Kelly-Bootle',
+	'> Always code as if the guy who ends up maintaining your code will be a violent psychopath who knows where you live. - Rick Osborne',
+	'> Any fool can write code that a computer can understand. Good programmers write code that humans can understand. - Martin Fowler',
+	'> Beware of bugs in the above code; I have only proved it correct, not tried it.  - Donald Knuth',
+	'> If we’d asked the customers what they wanted, they would have said “faster horses” — Henry Ford',
+	'> I (…) am rarely happier than when spending an entire day programming my computer to perform automatically a task that would otherwise take me a good ten seconds to do by hand. - Douglas Adams, Last Chance to See',
+	'> I will, in fact, claim that the difference between a bad programmer and a good one is whether he considers his code or his data structures more important. Bad programmers worry about the code. Good programmers worry about data structures and their relationships.  — Linus Torvalds',
+	'> Some problems are so complex that you have to be highly intelligent and well informed just to be undecided about them. — Laurence J. Peter',
+	'> Any intelligent fool can make things bigger, more complex, and more violent. It takes a touch of genius — and a lot of courage — to move in the opposite direction. — Albert Einstein',
+	'> Software obeys the law of gaseous expansion – it continues to grow until memory is completely filled. — Larry Gleason',
+	'> An organisation that treats its programmers as morons will soon have programmers that are willing and able to act like morons only. — Bjarne Stroustrup',
+	'> I have always wished that my computer would be as easy to use as my telephone. My wish has come true. I no longer know how to use my telephone. — Bjarne Stroustrup',
+	'> As soon as we started programming, we found to our surprise that it wasn’t as easy to get programs right as we had thought. Debugging had to be discovered. I can remember the exact instant when I realized that a large part of my life from then on was going to be spent in finding mistakes in my own programs.  — Maurice Wilkes discovers debugging, 1949',
+	'> Software gets slower faster than hardware gets faster. — Wirth’s law',
+	'> OOP is to writing a program, what going through airport security is to flying. — Richard Mansfield',
+	'> The problem with object-oriented languages is they’ve got all this implicit environment that they carry around with them. You wanted a banana but what you got was a gorilla holding the banana and the entire jungle. — Joe Armstrong',
+	'> IDE features are language smells. — Reg Braithwaite',
+	'> When there is no type hierarchy you don’t have to manage the type hierarchy. — Rob Pike',
+	'> Languages that try to disallow idiocy become themselves idiotic. — Rob Pike',
+	'> Premature optimization, that’s like a fart. Premature abstraction is like taking a dump on another developer’s desk. - Chris Eric',
 	'Benchmarks don’t lie, but liars do benchmarks.',
 
 	// food for thought by rlemon
-	'Can fat people go skinny dipping?',
-	'If quizzes are quizzical then why are tests not testical?',
 	'Can you be a closet claustrophobic?',
-	'What are Preparation A through Preparation G?',
 	'Is a metaphor like a simile?',
 	'How do you remove a club soda stain?',
-	'If practice makes perfect, and no one is perfect, why bother practice?',
-	'Why does the arcade game "Donkey Kong" have a monkey? Why isn\'t it called Monkey Kong?',
 	'If a rabbit\'s foot was actually lucky, wouldn\'t it still be attached to the rabbit\'s leg?',
-	'If a chicken had lips, could it whistle?',
-	'How much wind could a windbreaker break if a windbreaker could break wind?',
-	'Does expecting the unexpected make the unexpected expected?',
 	'If you had three quarters, four dimes and four pennies in your pocket you would have $1.19. You would also have the largest number (11) and combination of coins possible without being able to provide change for a dollar.',
-	'Where in the nursery rhyme does it say Humpty Dumpty is an egg?',
-	'How do vampires have such well-kept hair if they can\'t see themselves in the mirror?',
+	'Where in the nursery rhyme does it say Humpty Dumpty is an egg?'
 ];
 
 //query xkcd to find the last comic id. generate links to comic pages from that
@@ -5322,7 +5303,7 @@ function finishXKCD ( resp ) {
 
 	//js does not allow dynamic key creation on object literals
 	var props = {};
-	for (var i = 0; i < 3; i++) {
+	for (var i = 0; i < 5; i++) {
 		props[ responses.length + i ] = descriptor;
 	}
 
