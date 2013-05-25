b27f3adad86b2374a671ec97fb3081a38e33a016
Zirak <benstav@gmail.com>
2013-05-25 01:41:24 +0000

    added room ownership: see #62. phew

---
+++ source/plugins/roomPermissions.js
@@ -0,0 +1,154 @@
+(function () {
+"use strict";
+var ownerRoom = 17;
+
+if ( bot.adapter.roomid !== ownerRoom ) {
+	return;
+}
+
+var muted = JSON.parse( localStorage.bot_muted || '{}' );
+
+function giveVoice ( id, cb ) {
+	bot.log( 'giving voice to ' + id );
+
+	IO.xhr({
+		method : 'POST',
+		url : '/rooms/setuseraccess/' + ownerRoom,
+		data : {
+			aclUserId : id,
+			fkey : bot.adapter.fkey,
+			userAccess : 'read-write'
+		},
+
+		complete : finish
+	});
+
+	function finish () {
+		delete muted[ id ];
+		console.dir( muted );
+		localStorage.bot_muted = JSON.strict( muted );
+		cb && ( cb.apply(null, arguments) );
+	}
+}
+function takeVoice ( id, reason, cb ) {
+	bot.log( 'taking voice from %s because: %s', id, reason );
+	IO.xhr({
+		method : 'POST',
+		url : '/rooms/setuseraccess/' + ownerRoom,
+		data : {
+			aclUserId : id,
+			fkey : bot.adapter.fkey,
+			userAccess : 'remove'
+		},
+
+		complete : finish
+	});
+
+	function finish () {
+		muted[ id ] = { reason : reason };
+		console.dir( muted );
+		localStorage.bot_muted = JSON.stringify( muted );
+
+		cb.apply( null, arguments );
+	}
+}
+
+function muteList () {
+	return Object.keys( muted ).map(function ( k ) {
+		return k + ' (' + muted[ k ].reason + ')';
+	}).join( '; ' );
+}
+
+IO.register( 'userjoin', function ( msgObj ) {
+	var id = msgObj.user_id,
+		room = msgObj.room_id;
+
+	if ( room !== ownerRoom || muted[id] ) {
+		return;
+	}
+
+	giveVoice( id );
+});
+
+bot.addCommand({
+	name : 'mute',
+	fun : function mute ( args ) {
+		var parts = args.parse();
+
+		if ( !parts.length ) {
+			return muteList();
+		}
+		else if ( parts.length < 2 ) {
+			return 'Please give mute reason, see `/help mute`';
+		}
+
+		bot.log( parts, '/mute input' );
+
+		var id = parts[ 0 ],
+			name;
+		if ( /\D/.test(id) ) {
+			name = id;
+			id = args.findUserid( id );
+		}
+		if ( id < 0 ) {
+			return 'User ' + name + ' not found';
+		}
+
+		takeVoice( id, parts[1], finish );
+
+		function finish () {
+			var msg = 'Muted user ' + id;
+			if ( name ) {
+				msg += ' (' + name + ')';
+			}
+
+			args.reply( msg );
+		}
+	},
+
+	permission : {
+		del : 'NONE',
+		use : 'OWNER'
+	},
+	description : 'Mutes a user. `/mute usrid reason`'
+});
+
+bot.addCommand({
+	name : 'unmute',
+	fun : function umute ( args ) {
+		var id = args.parse()[ 0 ],
+			name;
+
+		bot.log( id, '/unmute input' );
+
+		if ( !id ) {
+			return 'Who shall I unmute?';
+		}
+
+		if ( /\D/.test(id) ) {
+			name = id;
+			id = args.findUserid( id );
+		}
+		if ( id < 0 ) {
+			return 'User ' + name + ' not found';
+		}
+
+		giveVoice( id, finish );
+
+		function finish () {
+			var msg = 'Unmuted user ' + id;
+			if ( name ) {
+				msg += ' (' + name + ')';
+			}
+			args.reply( msg );
+		}
+	},
+
+	permissions : {
+		del : 'NONE',
+		use : 'OWNER'
+	},
+	description : 'Unmutes a user. `/unmute usrid`'
+});
+
+})();
