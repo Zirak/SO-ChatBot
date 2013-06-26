fbe01a3ea7a20137c9886d9d3f07030194e49868
Zirak <benstav@gmail.com>
2013-06-26 23:41:34 +0000

    improved STOP internally to make more sense

--- source/plugins/STOP.js
+++ source/plugins/STOP.js
@@ -1,27 +1,25 @@
+(function () {
+var hammers = {
+	STOP  : 'HAMMERTIME!',
+	STAHP : 'HAMMAHTIME!',
+	HALT  : 'HAMMERZEIT!',
+	STOY  : 'ZABIVAT\' VREMYA!',
+	CAESUM: 'MALLEUS TEMPUS!'
+};
+
+// /(STOP|STAHP|...)[\.!\?]?$/
+var re = new RegExp(
+	'(' +
+		Object.keys(hammers).map(RegExp.escape).join('|')
+	+ ')[\.!\?]?$' );
+
 IO.register( 'input', function STOP ( msgObj ) {
 	var sentence = msgObj.content.toUpperCase(),
 		res;
 
-	//for probably good reason, it didn't allow me to apply the optional
-	// operator on beginnin-of-input, i.e. ^?
-	//so we have to wrap the ^ in parens
-	if ( /(^)?STOP[\.!\?]?$/.test(sentence) ) {
-		res = 'HAMMERTIME!';
-	}
-	else if ( /(^)?STAHP[\.!\?]?$/.test(sentence) ) {
-		res = 'HAMMAHTIME!';
-	}
-	else if ( /(^)?HALT[\.!\?]?$/.test(sentence) ) {
-		res = 'HAMMERZEIT!';
-	}
-    else if ( /(^)?STOY[\.!\?]?$/.test(sentence) ) {
-        res = 'ZABIVAT\' VREMYA!';
-    }
-    else if ( /(^)?CAESUM[\.!\?]?$/.test(sentence) ) {
-        res = 'MALLEUS TEMPUS!';
-    }
-
-	if ( res ) {
-		bot.adapter.out.add( res, msgObj.room_id );
+	if ( res = re.exec(sentence) ) {
+		bot.adapter.out.add( hammers[res[1]]  , msgObj.room_id );
 	}
 });
+
+})();
