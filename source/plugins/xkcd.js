4487c47c37196caae9bf8a5214aaa72766a18b36
Zirak <benstav@gmail.com>
2013-07-04 11:25:49 +0000

    cleaned /xkcd a bit to match dah groove

--- source/plugins/xkcd.js
+++ source/plugins/xkcd.js
@@ -1,48 +1,60 @@
 (function() {
+//Gets or sets a XKCD comic-type thing
+//just kidding! we can't set one. I'm just used to crappy javadoc style.
+//*sniffle*
 
-	// Gets a XKCD comic-type thing
+function getXKCD( args, cb ) {
+	var prop = ( args.parse()[0] || '' ).toLowerCase(),
+		linkBase = 'http://xkcd.com/';
 
-	function getXKCD( args, cb ) {
-		props = args.parse();
-		// They want a random XKCD, or the latest
-		if ( !props[0] || props[0] === 'new' ) {
+	//they want a specifix xkcd
+	if ( /\d{1,4}/.test(prop) ) {
+		finish( linkBase + prop );
+		return;
+	}
+	//we have no idea what they want. lazy arrogant bastards.
+	else if ( prop && prop !== 'new' ) {
+		finish( 'Clearly, you\'re not geeky enough for XKCD.' );
+		return;
+	}
 
-			IO.jsonp({
-				url : 'http://dynamic.xkcd.com/api-0/jsonp/comic',
-				jsonpName : 'callback',
-				fun : finishXKCD
-			});
+	//they want a random XKCD, or the latest
+	IO.jsonp({
+		url : 'http://dynamic.xkcd.com/api-0/jsonp/comic',
+		jsonpName : 'callback',
+		fun : finishXKCD
+	});
 
-			function finishXKCD ( resp ) {
-				var maxID = resp.num;
-				if ( !props[0] ) {
-					finish( 'http://xkcd.com/' + Math.rand( 1, maxID ));
-				} else {
-					finish( 'http://xkcd.com/' + maxID );
-				}
-			}
-		// They want a certian XKCD
-		} else if ( /\d{1,4}/.test(props[0]) ) {
-			finish( 'http://xkcd.com/' + props[0] );
-		} else {
-			finish( 'Clearly, you\'re not geeky enough for XKCD.' );
+	function finishXKCD ( resp ) {
+		var maxID = resp.num;
+		if ( !prop ) {
+			finish( linkBase + Math.rand(1, maxID) );
 		}
-
-		function finish( res ) {
-			bot.log( res, '/xkcd final' );
-			if ( cb && cb.call ) {
-				cb( res );
-			}
-			else {
-				args.directreply( res );
-			}
+		else {
+			finish( linkBase + maxID );
 		}
 	}
 
-	bot.addCommand({
-		name: 'xkcd',
-		fun: getXKCD,
-		description: 'Returns an XKCD.  Call with no args for random, `new` for latest, or a number for that comic',
-		async: true
-	});
+	function finish( res ) {
+		bot.log( res, '/xkcd finish' );
+
+		if ( cb && cb.call ) {
+			cb( res );
+		}
+		else {
+			args.directreply( res );
+		}
+	}
+}
+
+bot.addCommand({
+	name : 'xkcd',
+	fun : getXKCD,
+	permissions : {
+		del : 'NONE'
+	},
+	description : 'Returns an XKCD. Call with no args for random, ' +
+		'`new` for latest, or a number for a specific one.',
+	async : true
+});
 })();
