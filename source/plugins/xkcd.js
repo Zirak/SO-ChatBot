2b68483b0543d2559b06999094c8445ae08b986d
Ben Nelson <chess19@gmail.com>
2013-09-16 19:29:05 -0400

    Implemented xkcdSearch

--- source/plugins/xkcd.js
+++ source/plugins/xkcd.js
@@ -61,39 +61,24 @@ bot.addCommand({
 
 
 function searchXKCD( args, cb ) {
-	var prop = ( args.parse()[0] || '' ).toLowerCase(),
-		linkBase = 'http://xkcd.com/';
-
-	//they want a specifix xkcd
-	if ( /\d{1,4}/.test(prop) ) {
-		finish( linkBase + prop );
-		return;
-	}
-	//we have no idea what they want. lazy arrogant bastards.
-	else if ( prop && prop !== 'new' ) {
-		finish( 'Clearly, you\'re not geeky enough for XKCD.' );
-		return;
-	}
-
-	//they want a random XKCD, or the latest
-	IO.jsonp({
-		url : 'http://dynamic.xkcd.com/api-0/jsonp/comic',
-		jsonpName : 'callback',
-		fun : finishXKCD
-	});
-
-	function finishXKCD ( resp ) {
-		var maxID = resp.num;
-
-		if ( !prop ) {
-			finish( linkBase + Math.rand(1, maxID) );
+	IO.jsonp.google(
+		args.toString() + ' site:xkcd.com -forums.xkcd -m.xkcd', finishCall);
+        
+    function finishCall( resp ) {
+        if ( resp.responseStatus !== 200 ) {
+			finish( 'Something went on fire; status ' + resp.responseStatus );
+			return;
 		}
-		else if ( prop === 'new' ) {
-			finish( linkBase + maxID );
-		}
-	}
-
-	function finish( res ) {
+		var result = resp.responseData.results[ 0 ];
+        var matches = /xkcd.com\/(\d+)/.exec(result.url);
+        if(!matches) {
+            finish( 'Search didn\'t yield a comic; yielded: ' +result.url);
+            return;
+        }
+        finish(getXKCD(matches[1]), cb);
+    }
+    
+    function finish( res ) {
 		bot.log( res, '/xkcd finish' );
 
 		if ( cb && cb.call ) {
@@ -111,7 +96,7 @@ bot.addCommand({
 	permissions : {
 		del : 'NONE'
 	},
-	description : 'Returns an XKCD, based on query.',
+	description : 'Returns an XKCD, based on query to google.',
 	async : true
 });
 })();
