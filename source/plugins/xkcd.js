af7c408c53e98cb810d4d29d1f21dbf229cf0931
Ben Nelson <chess19@gmail.com>
2013-09-16 19:49:34 -0400

    Missed paren

--- source/plugins/xkcd.js
+++ source/plugins/xkcd.js
@@ -75,7 +75,7 @@ function searchXKCD( args, cb ) {
             finish( 'Search didn\'t yield a comic; yielded: ' +result.url);
             return;
         }
-        finish(getXKCD(bot.Message(matches[1], args.get()), cb);
+        finish(getXKCD(bot.Message(matches[1], args.get())), cb);
     }
     
     function finish( res ) {
