80767d27b896e079faa8b47d7bf7941e303e2e44
Kit Fox <foxfire.kitty@gmail.com>
2013-10-22 10:37:37 -0400

    added chatuser function
    Copied the user function and substituted chat address, to see what happens.

--- master.js
+++ master.js
@@ -1982,6 +1982,22 @@ var commands = {
 		}
 
 		args.directreply( 'http://stackoverflow.com/users/' + id );
+		
+	chatuser : function ( args ) {
+		var props = args.parse(),
+			usrid = props[ 0 ] || args.get( 'user_id' ),
+			id = usrid;
+
+		//check for searching by username
+		if ( !(/^\d+$/.test(usrid)) ) {
+			id = args.findUserid( usrid );
+
+			if ( id < 0 ) {
+				return 'Can\'t find user ' + usrid + ' in this chatroom.';
+			}
+		}
+
+		args.directreply( 'http://chat.stackexchange.com/users/' + id );
 	}
 };
 
