f40b7eddd5a7706c12b3df8c4a6ce130f417bfd9
Zirak <benstav@gmail.com>
2013-11-15 20:36:06 +0000

    removing auto-welome, fixes #117

--- source/plugins/welcome.js
+++ source/plugins/welcome.js
@@ -1,8 +1,5 @@
 (function () {
 "use strict";
-//welcomes new users with a link to the room rules
-
-var seen = bot.memory.get( 'users' );
 
 var message = "Welcome to the JavaScript chat! Please review the " +
 		bot.adapter.link(
@@ -15,51 +12,6 @@ function welcome ( name, room ) {
 	bot.adapter.out.add(
 		bot.adapter.reply( name ) + " " + message, room );
 }
-
-IO.register( 'userregister', function ( user, room ) {
-	var semiLegitUser = bot.isOwner( user.id ) ||
-		user.reputation > 1000 || user.reputation < 20;
-
-	if (
-		Number( room ) !== 17 || semiLegitUser  || seen[ user.id ]
-	) {
-		if ( semiLegitUser ) {
-			finish( true );
-		}
-		return;
-	}
-
-	IO.xhr({
-		method : 'GET',
-		url : '/users/' + user.id,
-
-		complete : complete
-	});
-
-	function complete ( resp ) {
-		//I'm parsing html with regexps. hopefully Cthulu won't eat me.
-		// <a href="/transcript/17">7</a>
-		// <a href="/transcript/17">47.1k</a>
-		var chatMessages = /transcript\/17(?:'|")>([\d\.]+)(k?)/.exec( resp );
-
-		if ( !chatMessages || (
-			!chatMessages[ 2 ] || parseFloat( chatMessages[1] ) < 2
-		)) {
-			welcome( user.name, room );
-		}
-		finish();
-	}
-
-	function finish ( unsee ) {
-		if ( unsee ) {
-			delete seen[ user.id ];
-		}
-		else {
-			seen[ user.id ] = true;
-		}
-		bot.memory.save( 'users' );
-	}
-});
 
 bot.addCommand({
 	name : 'welcome',
