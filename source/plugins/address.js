3ed4faac55453dc223bde819d21aa6654d8991a4
Shea Sollars <andrew-jackson@users.noreply.github.com>
2013-11-19 16:46:25 -0800

    Now solves #65
    Where `@BotName2342345 !!commandName`, `!!` is now optional

--- source/plugins/address.js
+++ source/plugins/address.js
@@ -5,8 +5,13 @@ var botName = bot.users[ bot.adapter.user_id ].name,
 IO.register( 'input', function ( msgObj ) {
 	var message = msgObj.content;
 
-	if ( message.indexOf(addresser + bot.invocationPattern) === 0 ) {
-		msgObj.content = message.substring( addresser.length );
+	if ( message.indexOf(addresser) === 0 ) {
+		message = msgObj.content = message.substring( addresser.length );
+
+		if ( message.indexOf(bot.invocationPattern) !== 0 ) {
+			msgObj.content = bot.invocationPattern + message;
+		}
+
 		bot.parseMessage( msgObj );
 	}
 });
