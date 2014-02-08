0e140e5dacf18a108939fde45d1954beb64d04b5
Zirak <benstav@gmail.com>
2014-02-08 17:42:18 +0200

    define now follows some alternatives and linkifies; fixes #175

--- source/plugins/define.js
+++ source/plugins/define.js
@@ -5,27 +5,34 @@
 
 var notFoundMsgs = [
 	'No definition found.',
-	'It means I aint got time to learn your $5 words',
+	'It means I aint got time to learn your $5 words.',
 	'My pocket dictionary just isn\'t good enough for you.'
 ];
+var wikiUrl = 'http://en.wiktionary.org';
+//I wish regexps had the x flag...
+/*
+  ( ... )    # the category: alternative spelling, common missspelling, etc
+   (of|for)  # alternative spelling of, aternative term for
+  (.+?)\.?   # what this shit is an alternative of, sometimes followed by a dot
+*/
+var alternativeRe = /(alternative (spelling|term)|common misspelling|informal form|archaic spelling) (of|for) (.+?)\.?$/i;
 
 var define = {
 	command : function defineCommand ( args, cb ) {
 		bot.log( args, '/define input' );
-		this.fetchData( args, finish );
+		this.fetchData( args.toString(), finish );
 
-		function finish ( results, pageid ) {
-			bot.log( results, '/define results' );
-			//TODO: format. so far we just be lazy and take the first one
-			var res = results[ 0 ];
+		function finish ( definition, pageid ) {
+			bot.log( definition, pageid, '/define result' );
+			var res;
 
-			if ( !res ) {
+			if ( pageid < 0 ) {
 				res = notFoundMsgs.random();
 			}
 			else {
 				res = bot.adapter.link(
-					args, 'http://en.wiktionary.org/wiki?curid=' + pageid
-				) + ' ' + res;
+					definition.name, wikiUrl + '/wiki?curid=' + pageid
+				) + ' ' + definition.text;
 			}
 
 			if ( cb && cb.call ) {
@@ -40,18 +47,99 @@ var define = {
 	handleResponse : function ( resp, cb ) {
 		var query = resp.query,
 			pageid = query.pageids[ 0 ],
-			html = query.pages[ pageid ].extract;
+			page = query.pages[ pageid ],
+			html = page.extract;
 
 		if ( pageid === '-1' ) {
-			cb( [], -1 );
+			cb( {}, -1 );
 			return;
 		}
 
 		var root = document.createElement( 'body' );
 		root.innerHTML = html; //forgive me...
+		var definition  = this.extractDefinition( root );
 
-		//the first ol has all the data we need
-		cb( getEvents(root.getElementsByTagName('ol')[0]), pageid );
+		//if this is an alternative definition (or spelling, or whatever),
+		// return the actual version.
+		if ( definition.alternative ) {
+			bot.log( definition.alternative, '/define found alternative' );
+			this.fetchData( definition.alternative, cb );
+		}
+		else {
+			cb({
+				name : page.title,
+				text : definition.text
+			});
+		}
+	},
+
+	extractDefinition : function ( root ) {
+		/*
+		Result of 42:
+			<ol>
+				<li>The cardinal number forty-two.</li>
+			</ol>
+
+		Result of plugin:
+			<ol>
+				<li>
+					<span class="use-with-mention">
+						Alternative spelling of
+						<i class="Latn mention" lang="en" xml:lang="en">
+							<a href="/wiki/plug-in#English" title="plug-in">
+								plug-in
+							</a>
+						</i>
+					</span>
+					.
+				</li>
+			</ol>
+
+		Result of puling:
+			<ol>
+				<li>
+					<span class="use-with-mention">
+						Present participle of
+						<i class="Latn mention" lang="en" xml:lang="en">
+							<a href="/wiki/pule#English" title="pule">
+								pule
+							</a>
+						</i>
+					</span>
+					.
+				</li>
+			</ol>
+		*/
+		var defList = root.getElementsByTagName( 'ol' )[ 0 ],
+			defElement = defList.firstElementChild,
+			links = defElement.getElementsByTagName( 'a' );
+
+		//before we start messing around with the element's innards, try and
+		// find if it's an alternative of something else.
+		var alternative = this.extractAlternative( defElement.textContent );
+
+		//be sure to replace links with formatted links.
+		while ( links.length ) {
+			replaceLink( links[0] );
+		}
+
+		return {
+			alternative : alternative,
+			text : defElement.textContent
+		};
+
+		function replaceLink ( link ) {
+			var href = wikiUrl + link.getAttribute( 'href' ),
+				textLink = bot.adapter.link( link.textContent, href ),
+
+				textNode = document.createTextNode( textLink );
+
+			link.parentNode.replaceChild( textNode, link );
+		}
+	},
+
+	extractAlternative : function ( definitionText ) {
+		return ( alternativeRe.exec(definitionText) || [] ).pop();
 	},
 
 	fetchData : function ( term, cb ) {
@@ -62,7 +150,7 @@ var define = {
 			jsonpName : 'callback',
 			data : {
 				action : 'query',
-				titles : term.toString(),
+				titles : term,
 				format : 'json',
 				prop : 'extracts',
 				indexpageids : true
@@ -74,49 +162,11 @@ var define = {
 	}
 };
 
-//example of partial extract:
-/*
-  <h2> Translingual</h2>\n\n
-  <p>Wikipedia</p>\n
-  <h3> Symbol</h3>\n
-  <p><b>42</b> (<i>previous</i>  <b>41</b>, <i>next</i>  <b>43</b>)</p>\n
-  <ol>
-      <li>The cardinal number forty-two.</li>\n</ol>
-*/
-//we just want the li data
-function getEvents ( root, stopNode ) {
-	var matches = [];
-
-	(function filterEvents (root) {
-		var node = root.firstElementChild;
-
-		for (; node; node = node.nextElementSibling) {
-			if (node === stopNode) {
-				return;
-			}
-			else if (node.tagName !== 'LI' ) {
-				continue;
-			}
-
-			matches.push( node );
-		}
-	})( root );
-
-	//we need to flatten out the resulting elements, and we're done!
-	return flatten(matches);
-}
-function flatten ( lis ) {
-	return [].map.call( lis, extract );
-
-	function extract ( li ) {
-		return li.firstChild.data;
-	}
-}
-
 bot.addCommand({
 	name : 'define',
 	fun : define.command,
 	thisArg : define,
+
 	permissions : {
 		del : 'NONE'
 	},
