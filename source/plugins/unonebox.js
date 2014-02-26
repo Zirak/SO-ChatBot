e5e588066934c4401e5f587e9ee8bc7693e3b781
Robert Lemon <rob.lemon@gmail.com>
2014-02-26 10:12:19 -0500

    updating unonebox

--- source/plugins/unonebox.js
+++ source/plugins/unonebox.js
@@ -63,7 +63,10 @@ var unonebox = {
 
 		var frag = document.createElement( 'div' );
 		frag.innerHTML = msgObj.content;
-		var link = frag.querySelector( '.onebox a' );
+		// do not un-onebox youtube videos and quotes 
+		// .content > is required because quotes of images (!!artisticpoop) 
+		// have a nested .onebox element inside of them
+		var link = frag.querySelector( '.content > .onebox:not(.ob-youtube,.ob-message) a' );
 
 		// No onebox, no un-oneboxing.
 		if ( !link ) {
