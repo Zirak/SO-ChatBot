79ca4daa0c1f9aee861ea73a43cd9ccd364610bc
Zirak <zirak@mail.com>
2014-06-07 21:08:16 +0300

    Removed awsm, color, domain, findCommand, github, history, meme, norris, parse, timer, todo and zalgo. Deprecated convert and user. Closes #193, closes #198, closes #202. See https://gist.github.com/Zirak/f1883eedd560bff51107.

--- source/plugins/user.js
+++ source/plugins/user.js
@@ -1,20 +1,7 @@
 bot.addCommand({
 	name : 'user',
-	fun : function ( args ) {
-		var props = args.parse(),
-			usrid = props[ 0 ] || args.get( 'user_id' ),
-			id = usrid;
-
-		//check for searching by username
-		if ( !(/^\d+$/.test(usrid)) ) {
-			id = args.findUserId( usrid );
-
-			if ( id < 0 ) {
-				return 'Can\'t find user ' + usrid + ' in this chatroom.';
-			}
-		}
-
-		args.directreply( 'http://stackoverflow.com/users/' + id );
+	fun : function () {
+		return 'Command deprecated. If you want it to stay, ping Zirak.';
 	},
 	permissions : { del : 'NONE', use : 'ALL' },
 	description : 'Fetches user-link for specified user. ' +
