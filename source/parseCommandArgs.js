435349dbd5152ce091e6c02d2068fce45db54ef2
Zirak <zirak@mail.com>
2014-09-19 14:57:19 +0300

    Replaced command argument parser.
    It's simpler now, only accepts double-quotes, more lenient. Fixes #214.

--- source/parseCommandArgs.js
+++ source/parseCommandArgs.js
@@ -1,206 +1,77 @@
 (function () {
 "use strict";
 
-var target;
-if ( typeof bot !== 'undefined' ) {
-	target = bot;
-}
-else if ( typeof exports !== 'undefined' ) {
-	target = exports;
-}
-else {
-	target = window;
-}
+var argParser = {
+	create : function () {
+		var ret = Object.create(this);
 
-target.parseCommandArgs = (function () {
+		ret.separator = ' ';
+		ret.escape = '\\';
+		ret.quote = '"';
 
-//the different states, not nearly enough to represent a female humanoid
-//you know you're building something fancy when it has constants with
-// undescores in their name
-var S_DATA         = 0,
-	S_SINGLE_QUOTE = 1,
-	S_DOUBLE_QUOTE = 2,
-	S_NEW          = 3;
-
-//and constants representing constant special chars (why aren't I special? ;_;)
-var CH_SINGLE_QUOTE = '\'',
-	CH_DOUBLE_QUOTE = '\"';
-
-/*
-the "scheme" roughly looks like this:
-  args -> arg <sep> arg <sep> arg ... | Ø
-  arg  -> singleQuotedString | doubleQuotedString | string | Ø
-
-  singleQuotedString -> 'string'
-  doubleQuotedString -> "string"
-  string -> char char char ... | Ø
-  char -> anyCharacter | <escaper>anyCharacter | Ø
-
-Ø is the empty string
-*/
-
-//the bad boy in the hood
-//I dunno what kind of parser this is, so I can't flaunt it or taunt with it,
-// but it was fun to make
-var parser = {
-
-	parse : function ( source, sep, esc ) {
-		//initializations are safe fun for the whole family!
-		//later-edit: the above comment is one of the weirdest I've ever
-		// written
-		this.source = source;
-		this.pos = 0;
-		this.length = source.length;
-		this.state = S_DATA;
-		this.lookahead = '';
-
-		this.escaper = esc || '~';
-		this.separator = sep || ' ';
-
-		var args = this.tokenize();
-
-		//oh noez! errorz!
-		if ( this.state !== S_DATA ) {
-			this.throwFinishError();
-		}
-
-		return args;
+		return ret;
 	},
 
-	tokenize : function () {
-		var arg, ret = [];
+	parse : function (source) {
+		this.source = source;
+		this.pos = 0;
 
-		//let the parsing commence!
-		while ( this.pos < this.length ) {
-			arg = this.nextArg();
+		var ret = [];
 
-			//only add the next arg if it's actually something
-			if ( arg ) {
-				ret.push( arg );
-			}
+		while (!this.done()) {
+			ret.push(this.nextArg());
 		}
 
 		return ret;
 	},
 
-	//fetches the next argument (see the "scheme" at the top)
 	nextArg : function () {
-		var lexeme = '', ch;
-		this.state = S_DATA;
+		var endChar = this.separator;
 
-		while ( true ) {
-			ch = this.nextChar();
-			if ( ch === null || this.state === S_NEW ) {
-				break;
+		if (this.peek() === this.quote) {
+			this.nextChar();
+			endChar = this.quote;
+		}
+
+		return this.consumeUntil(endChar);
+	},
+
+	consumeUntil : function (endChar) {
+		var char = this.nextChar(),
+			escape = false,
+			ret = '';
+
+		while (char && char !== endChar) {
+			if (char === this.escape && !escape) {
+				escape = true;
+			}
+			else {
+				ret += char;
 			}
 
-			lexeme += ch;
+			char = this.nextChar();
 		}
 
-		return lexeme;
+		return ret;
 	},
 
-	nextChar : function ( escape ) {
-		var ch = this.lookahead = this.source[ this.pos ];
-		this.pos++;
-
-		if ( !ch ) {
-			return null;
-		}
-
-		if ( escape ) {
-			return ch;
-		}
-
-		//l'escaping!
-		else if ( ch === this.escaper ) {
-			return this.nextChar( true );
-		}
-
-		//encountered a separator and you're in data-mode!? ay digity!
-		else if ( ch === this.separator && this.state === S_DATA ) {
-			this.state = S_NEW;
-			return ch;
-		}
-
-		return this.string();
+	nextChar : function () {
+		var ret = this.source[this.pos];
+		this.pos += 1;
+		return ret;
 	},
 
-	//IM IN YO STRINGZ EATING YO CHARS
-	// a.k.a string handling starts roughly here
-	string : function () {
-		var ch = this.lookahead;
-
-		//single quotes are teh rulez
-		if ( ch === CH_SINGLE_QUOTE ) {
-			return this.singleQuotedString();
-		}
-
-		//exactly the same, just with double-quotes, which aren't quite as teh
-		// rulez
-		else if ( ch === CH_DOUBLE_QUOTE ) {
-			return this.doubleQuotedString();
-		}
-
-		return ch;
+	peek : function () {
+		return this.source[this.pos];
 	},
 
-	singleQuotedString : function () {
-		//we're already inside a double-quoted string, it's just another
-		// char for us
-		if ( this.state === S_DOUBLE_QUOTE ) {
-			return this.lookahead;
-		}
-
-		//start your stringines!
-		else if ( this.state !== S_SINGLE_QUOTE ) {
-			this.state = S_SINGLE_QUOTE;
-		}
-
-		//end your stringiness!
-		else {
-			this.state = S_DATA;
-		}
-
-		return this.nextChar();
-	},
-
-	doubleQuotedString : function () {
-		if ( this.state === S_SINGLE_QUOTE ) {
-			return this.lookahead;
-		}
-
-		else if ( this.state !== S_DOUBLE_QUOTE ) {
-			this.state = S_DOUBLE_QUOTE;
-		}
-
-		else {
-			this.state = S_DATA;
-		}
-
-		return this.nextChar();
-	},
-
-	throwFinishError : function () {
-		var errMsg = '';
-
-		if ( this.state === S_SINGLE_QUOTE ) {
-			errMsg = 'Expected ' + CH_SINGLE_QUOTE;
-		}
-		else if ( this.state === S_DOUBLE_QUOTE ) {
-			errMsg = 'Expected ' + CH_DOUBLE_QUOTE;
-		}
-
-		var up = new Error( 'Unexpected end of input: ' + errMsg );
-		up.column = this.pos;
-
-		throw up; //problem?
+	done : function () {
+		return this.pos >= this.source.length;
 	}
 };
 
-return function () {
-	return parser.parse.apply( parser, arguments );
-};
-}());
+
+var parser = bot.commandArgsParser = argParser.create();
+bot.parseCommandArgs = parser.parse.bind(parser);
 
 }());
