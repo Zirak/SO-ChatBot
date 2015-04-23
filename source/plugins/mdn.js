9f02262d02bdf23835c7ce1fdb70a0bf353af56d
Awal Garg <awalGarg@users.noreply.github.com>
2015-04-24 02:32:50 +0530

    prettier urls and multiple arguments
    i tested in sandbox and it worked for me

--- source/plugins/mdn.js
+++ source/plugins/mdn.js
@@ -1,10 +1,14 @@
 (function () {
 
 function mdn ( args, cb ) {
-	IO.jsonp.google(
-		args.toString() + ' site:developer.mozilla.org', finishCall );
-
+	var terms = args.toString().split(/,\s*/g),
+		results = {'unescapedUrls': [], 'formatted': []};
+	terms.forEach(function (term) {
+		IO.jsonp.google(
+			term + ' site:developer.mozilla.org', finishCall );
+	});
 	function finishCall ( resp ) {
+
 		if ( resp.responseStatus !== 200 ) {
 			finish( 'Something went on fire; status ' + resp.responseStatus );
 			return;
@@ -12,7 +16,19 @@ function mdn ( args, cb ) {
 
 		var result = resp.responseData.results[ 0 ];
 		bot.log( result, '/mdn result' );
-		finish( result.url );
+		
+		var title = result.titleNoFormatting.replace(/ -.+/, '');
+		results.formatted.push(bot.adapter.link(IO.decodehtmlEntities(title), result.url));
+		results.unescapedUrls.push(result.url);
+		
+		if (results.formatted.length === terms.length) {
+			var msg = results.formatted.join(', ');
+			if (msg.length > bot.adapter.maxLineLength) {
+				msg = results.unescapedUrls.join(', ');
+			}
+			finish( msg );
+		}
+
 	}
 
 	function finish ( res ) {
