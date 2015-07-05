ae335508689b350e995845be8b8caff0efc7d4c3
Like wot <likewot@users.noreply.github.com>
2015-07-05 12:22:18 +0530

    rawgithub is now rawgit

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,1 +1,1 @@
-(function(){var a=document.createElement("script");a.src="https://rawgithub.com/Zirak/SO-ChatBot/master/master.js",document.head.appendChild(a)})()
+(function(){var a=document.createElement("script");a.src="https://rawgit.com/Zirak/SO-ChatBot/master/master.js",document.head.appendChild(a)})()
