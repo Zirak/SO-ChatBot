29d95286620c685f7c62af63761463d04f18ef6f
rlemon <rob.lemon@gmail.com>
2015-08-12 09:59:24 -0400

    adding imdb

---
+++ source/plugins/imdb.js
@@ -0,0 +1,62 @@
+(function () {
+
+function imdb ( args, cb ) {
+   var terms = args.toString().split(/,\s*/g);
+   var results = {
+      unescapedUrls : [],
+      formatted : []
+   };
+
+   terms.forEach(function ( term ) {
+      IO.jsonp.google(
+         term + ' site:imdb.com', finishCall );
+   });
+
+   function finishCall ( resp ) {
+      if ( resp.responseStatus !== 200 ) {
+         finish( 'Something went on fire; status ' + resp.responseStatus );
+         return;
+      }
+
+      var result = resp.responseData.results[ 0 ];
+      bot.log( result, '/imdb result' );
+
+      var title = IO.decodehtmlEntities(
+         result.titleNoFormatting.split(' -')[0].trim()
+      );
+
+      results.formatted.push( bot.adapter.link(title, result.url) );
+      results.unescapedUrls.push( result.url );
+
+      if ( results.formatted.length === terms.length ) {
+         aggregatedResults();
+      }
+   }
+   function aggregatedResults () {
+      var msg = results.formatted.join( ', ' );
+      if ( msg.length > bot.adapter.maxLineLength ) {
+         msg = results.unescapedUrls.join( ', ' );
+      }
+
+      finish( msg );
+   }
+   function finish ( res ) {
+      if ( cb && cb.call ) {
+         cb( res );
+      }
+      else {
+         args.reply( res );
+      }
+   }
+};
+
+bot.addCommand({
+   name : 'imdb',
+   fun : imdb,
+
+   permissions : { del : 'NONE', use : 'ALL' },
+   description : 'Fetches imdb page. `/imdb what`',
+   async : true
+});
+
+})();
