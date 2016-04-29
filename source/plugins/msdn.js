81229f9bfbec221ed7a6675a80b862b334e54186
Robert Lemon <rob.lemon@gmail.com>
2016-04-29 10:51:28 -0400

    Create msdn.js

---
+++ source/plugins/msdn.js
@@ -0,0 +1,62 @@
+module.exports = function (bot) {
+
+    function msdn (args, cb) {
+        var terms = args.toString().split(/,\s*/g);
+
+        var results = {
+            unescapedUrls: [],
+            formatted: []
+        };
+
+        terms.forEach(function (term) {
+            bot.IO.jsonp.google(term + ' site:msdn.microsoft.com', finishCall);
+        });
+
+        function finishCall (resp) {
+            if (resp.responseStatus !== 200) {
+                finish('Something went on fire; status ' + resp.responseStatus);
+                return;
+            }
+
+            var result = resp.responseData.results[0];
+            bot.log(result, '/msdn result');
+
+            var title = bot.IO.decodehtmlEntities(
+                result.titleNoFormatting.split(' -')[0].trim()
+            );
+
+            results.formatted.push(bot.adapter.link(title, result.url));
+            results.unescapedUrls.push(result.url);
+
+            if (results.formatted.length === terms.length) {
+                aggregatedResults();
+            }
+        }
+        function aggregatedResults () {
+            var msg = results.formatted.join(', ');
+            if (msg.length > bot.adapter.maxLineLength) {
+                msg = results.unescapedUrls.join(', ');
+            }
+
+            finish(msg);
+        }
+        function finish (res) {
+            if (cb && cb.call) {
+                cb(res);
+            }
+            else {
+                args.reply(res);
+            }
+        }
+    }
+
+    bot.addCommand({
+        name: 'msdn',
+        fun: msdn,
+
+        permissions: { del: 'NONE', use: 'ALL' },
+        description: 'Fetches msdn page. `/msdn what`',
+        async: true
+    });
+
+};
